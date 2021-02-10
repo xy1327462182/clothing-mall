b11050ecb63cb027eadade1ca444d2345289e552
杨少博 <1327462182@qq.com>
2021-02-10 16:10:41 +0800

    搜索组件

---
+++ mp/components/Search/Search.js
@@ -0,0 +1,31 @@
+// components/Search/Search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    background: {
+      type: String,
+      value: '#ff2262'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+  
+  // 在组件定义时的选项中启用多slot支持
+  options: {
+    multipleSlots: true 
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
